5e268bafb4f47746d2af832d3e78ce4710cedf74
MartaNedomirko <marta.lviv.ua@gmail.com>
2017-10-13 17:36:15 +0300

    Upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './home/header/header.component';
+import { MainComponent } from './home/main/main.component';
+import { CreationsComponent } from './home/creations/creations.component';
+import { DarkComponent } from './home/dark/dark.component';
+import { NewsComponent } from './home/news/news.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    CreationsComponent,
+    DarkComponent,
+    NewsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
